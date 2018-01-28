0d60fe993bf518a25c586ad35d8f378ed43bc68e
mirifem <zippochina@gmail.com>
2018-01-29 02:16:32 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
 define('APIKEY', AIzaSyAcNvJvMw1KyAY9SobfsbyV9ZZLJZrRjWg);
-define('GJ_CODE', jp);
+define('GJ_CODE', tw);
 define('SITE_NAME', youtube);
 define('TITLENAME', youtube);
-define('EN2DEKEY', 1w34rS50Qzxv03Bbc);
-define('EMAIL', getenv("EMAIL"));
+define('EN2DEKEY', w34rS50Qzxv03Bbc);
+define('EMAIL', jnwcdma@gmail.com);
 ?>
